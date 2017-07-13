f8ff17e748fffaed8d550758f37087bf76e001b2
berenisser <berenisser@gmail.com>
2017-07-13 12:16:28 -0400

    Archivos enlazados y listos para partir

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-/* Holiii acá va tu código también */
+var llamarAjax = function(){
+	$.ajax({
+		url: '/path/to/file',
+		type: 'GET',
+		dataType: 'json',
+		data: {param1: 'value1'},
+	})
+	.done(function() {
+		console.log("success");
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+}
