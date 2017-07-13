252ee2dd15cf2a06ffff6713fac5c0e779544352
berenisser <berenisser@gmail.com>
2017-07-13 13:05:11 -0400

    Funcionalidad de Inicio listo, correo electronico guardado a local Storage

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 var llamarAjax = function(){
 	$.ajax({
-		url: '/path/to/file',
+		url: 'http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=123456',
 		type: 'GET',
 		dataType: 'json',
-		data: {param1: 'value1'},
+		data: {'limit': '2'},
 	})
-	.done(function() {
-		console.log("success");
+	.done(function(data) {
+		console.log(data);
 	})
 	.fail(function() {
 		console.log("error");
@@ -15,3 +15,49 @@ var llamarAjax = function(){
 		console.log("complete");
 	});
 }
+
+llamarAjax();
+
+   //Seccion Sign Up obtener Nombre y correo
+$(document).ready(function(){
+    $("#padre-btn-iniciar").on("click", $("#btn-iniciar"), function(e) {
+
+        $(".red").remove();
+
+        if(correo()){
+            if(contrasena()){
+                window.open('map.html','_self',false);  
+            }
+        }
+    });
+
+    //validación nombre
+    function contrasena(){
+        var nameValue = $("#contrasena").val();
+        console.log(nameValue);
+        if (!(/^[0-9]{2,8}$/).test(nameValue)){
+            $("#espacio-error-nombre").append('<p class="red">Contraseña debe incluir max 8 números</p>');
+            $("#contrasena").val("");
+            console.log("second");
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    //validacion correo 
+    function correo(){
+        var emailValue = $("#correo").val();
+        console.log(emailValue);
+        if (!(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/).test(emailValue)){
+            $("#espacio-error-nombre").append('<p class="red">Correo Invalido</p>');
+            $("#correo").val("");
+            console.log("first");
+            return false;
+        } else{
+            localStorage.setItem('email',emailValue);
+            return true;
+        }
+    }
+
+});
